6aea7896bd8464d7fe26b2b3b2bf88f8bce38666
franciscoy <francisco.yllera@gmail.com>
2015-02-17 18:16:19 -0300

    Add test for sorting

--- src/test/java/algorithm/sort/DefaultSortingTest.java
+++ src/test/java/algorithm/sort/DefaultSortingTest.java
@@ -2,7 +2,6 @@ package algorithm.sort;
 
 import org.junit.Test;
 
-import static algorithm.utils.ArrayUtils.printArr;
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
@@ -10,19 +9,27 @@ import static org.junit.Assert.assertArrayEquals;
  * Created by Francisco Yllera.
  */
 public abstract class DefaultSortingTest {
-    private static final int[] ONE_ELEMENT_DATASET = {1};
-    protected static final int[] UNSORTED_DATASET = {2,3,10,10,6,1,25,44,7};
-    protected static final int[] SORTED_DATASET = {1,2,3,6,7,10,10,25,44};
+    private final int[] oneElementArray = {1};
+    protected final int[] unsortedArray = {2,3,10,6,1,25,44,7};
+    protected final int[] sortedArray = {1,2,3,6,7,10,25,44};
+
+    protected final int[] unsortedArrayWithRepeatedElements = {2,3,10,10,1,6,1,25,44,2,7};
+    protected final int[] sortedArrayWithRepeatedElements = {1,1,2,2,3,6,7,10,10,25,44};
 
     protected abstract SortingAlgorithm getSortingAlgorithm();
 
     @Test
     public void testCorrectReturnWhenArrayContainsOneElement() {
-        assertEquals(getSortingAlgorithm().sort(ONE_ELEMENT_DATASET), ONE_ELEMENT_DATASET);
+        assertEquals(getSortingAlgorithm().sort(oneElementArray), oneElementArray);
     }
 
     @Test
     public void testCorrectSortingOfUnsortedArray() {
-        assertArrayEquals(getSortingAlgorithm().sort(UNSORTED_DATASET), SORTED_DATASET);
+        assertArrayEquals(getSortingAlgorithm().sort(unsortedArray), sortedArray);
+    }
+
+    @Test
+    public void testCorrectSortingOfUnsortedArrayWithRepeatedElements() {
+        assertArrayEquals(getSortingAlgorithm().sort(unsortedArray), sortedArray);
     }
 }
